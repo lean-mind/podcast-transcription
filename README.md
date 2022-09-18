408ea0a0863776e5033570e4b9042f39bca32ed8
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-09-18 14:12:43 +0100

    🚧 Added vosk integration but performance it is bad

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 ## 🧑‍💻 Install project
 
+> System installations
+
+```bash
+sudo apt install ffmpeg build-essential portaudio19-dev
+```
+
 > Requires Python 3.10
 
 ```bash
 make setup
-```
-
-> System installations
-
-```bash
-sudo apt install ffmpeg
 ```
 
 ## ❓Help
@@ -23,8 +23,9 @@ make help
 ## VOSK
 
 - [Website](https://alphacephei.com/vosk/)
-- [Models](https://alphacephei.com/vosk/models) 
+- [Models](https://alphacephei.com/vosk/models)
 
 ### ES Models
+
 - Small (39MB) : https://alphacephei.com/vosk/models/vosk-model-small-es-0.42.zip
 - Huge (1.4GB): https://alphacephei.com/vosk/models/vosk-model-es-0.42.zip
--- src/wav_to_text.py
+++ src/wav_to_text.py
@@ -1,7 +1,9 @@
-from os import listdir, path
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
+from os import listdir, path, remove
 import wget
+import zipfile
+import wave
+import json
+from vosk import Model, KaldiRecognizer
 
 
 class WavToText:
@@ -12,19 +14,42 @@ class WavToText:
         self.models_dir = f"{path.abspath('models')}/"
 
     def transform(self):
-        self._download_spanish_model()
         wav_files = [f for f in listdir(self.src_folder) if f.endswith('.wav')]
         for wav_file in wav_files:
             self._parse_to_text(wav_file)
 
     def _parse_to_text(self, wav_file):
-        ## TODO: Implement parsing to text format using VOSK
+        self._download_spanish_model()
+        model = self._build_model()
         abs_src_path = f"{self.src_folder}{wav_file}"
-        print(self.models_dir)
+        self._recognize_text(model, abs_src_path)
 
     def _download_spanish_model(self):
         url = "https://alphacephei.com/vosk/models/vosk-model-small-es-0.42.zip"
-        files_in_folder = [f for f in listdir(self.models_dir) if not f.startswith('.')]
-        if not files_in_folder:
-            response = wget.download(url, self.models_dir)
-            print(response)
+        language_models = [d for d in listdir(
+            self.models_dir) if path.isdir(f"{self.models_dir}{d}")]
+        if not language_models:
+            file_name = "vosk-model-es.zip"
+            wget.download(url, f"{self.models_dir}{file_name}")
+            self._unzip_model(file_name)
+
+    def _unzip_model(self, file_name: str) -> None:
+        abs_file_path = f"{self.models_dir}{file_name}"
+        with zipfile.ZipFile(abs_file_path, 'r') as zip_ref:
+            zip_ref.extractall(self.models_dir)
+            remove(abs_file_path)
+
+    def _build_model(self):
+        model_name = [d for d in listdir(
+            self.models_dir) if path.isdir(f"{self.models_dir}{d}")][0]
+        abs_model_path = f"{self.models_dir}{model_name}"
+        return Model(abs_model_path)
+
+    def _recognize_text(self, model, abs_file_path: str) -> None:
+        recognizer = KaldiRecognizer(model, 16000)
+        recognizer.SetWords(True)
+        wav = wave.open(abs_file_path, "rb")
+        print(wav.getnframes())
+        chunk = wav.readframes(400000)
+        if recognizer.AcceptWaveform(chunk):
+            print(recognizer.Result())
