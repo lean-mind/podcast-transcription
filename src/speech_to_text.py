3eee30dbdb08e11cff6154b7a0eb68930c1e06e6
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-09-26 17:58:37 +0100

    ♻️ Improved names

--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -17,16 +17,16 @@ class SpeechToText:
             self._save_text(podcast_content, mp3_filename)
 
     def _parse_audio_into_text(self, mp3_filename: str) -> str:
-        abs_src_path = f"{self.src_folder}{mp3_filename}"
+        audio_absolute_path = f"{self.src_folder}{mp3_filename}"
         print(f"PARSING FILE: {mp3_filename}")
         model = whisper.load_model(ModelSize.base) # Here we select the model size
-        result = model.transcribe(abs_src_path)
+        result = model.transcribe(audio_absolute_path)
         print(f"PARSED FILE: {mp3_filename}")
         return result["text"]
 
     def _save_text(self, podcast_content: str, mp3_filename: str):
-        abs_src_path = f"{self.dst_folder}{mp3_filename}".replace(".mp3", ".txt")
-        with open(abs_src_path, 'w') as file:
+        text_file_destination = f"{self.dst_folder}{mp3_filename}".replace(".mp3", ".txt")
+        with open(text_file_destination, 'w') as file:
             file.write(podcast_content)
-            print(f"CONTENT SAVED ON: {abs_src_path}")
+            print(f"CONTENT SAVED ON: {text_file_destination}")
             
