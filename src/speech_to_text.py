701b7ee297a53a375a57179e4eea9d26e0d74882
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-09-30 15:26:31 +0100

    ♻️ Improved names

--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -11,22 +11,21 @@ class SpeechToText:
         self.dst_folder = f"{dst_folder}/"
 
     def transform(self):
-        mp3_filenames = [f for f in listdir(self.src_folder) if f.endswith('.mp3')]
-        for mp3_filename in mp3_filenames:
-            podcast_content = self._parse_audio_into_text(mp3_filename)
-            self._save_text(podcast_content, mp3_filename)
+        filenames = [f for f in listdir(self.src_folder) if f.endswith('.mp3')]
+        [self._parse_speech_to_text(filename) for filename in filenames]
 
-    def _parse_audio_into_text(self, mp3_filename: str) -> str:
+    def _parse_speech_to_text(self, mp3_filename: str):
+        podcast_content = self._transcribe_audio_frames(mp3_filename)
+        self._save_text(podcast_content, mp3_filename)
+
+    def _transcribe_audio_frames(self, mp3_filename: str) -> str:
         audio_absolute_path = f"{self.src_folder}{mp3_filename}"
-        print(f"PARSING FILE: {mp3_filename}")
-        model = whisper.load_model(ModelSize.base) # Here we select the model size
+        model = whisper.load_model(ModelSize.tiny)  # Here we select the model size
         result = model.transcribe(audio_absolute_path)
-        print(f"PARSED FILE: {mp3_filename}")
         return result["text"]
 
     def _save_text(self, podcast_content: str, mp3_filename: str):
-        text_file_destination = f"{self.dst_folder}{mp3_filename}".replace(".mp3", ".txt")
+        text_file_destination = f"{self.dst_folder}{mp3_filename}".replace(
+            ".mp3", ".txt")
         with open(text_file_destination, 'w') as file:
             file.write(podcast_content)
-            print(f"CONTENT SAVED ON: {text_file_destination}")
-            
